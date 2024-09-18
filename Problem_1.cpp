c32c5d7fb3d52fd06d7079361b995f930d62b58b
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-09-18 20:15:18 +0800

    p1

---
+++ Problem_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[128] = {};
+    int i, t;
+    while(cin >> i){
+        for(int n=0; n<i; n++){
+            cin >> t;
+            arr[n] = t;
+        }
+        for(int l=1; l<=i; l++){
+            cout << arr[i-l];
+            if(l<i){
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
