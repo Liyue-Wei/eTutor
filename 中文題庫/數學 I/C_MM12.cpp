1b95d9a8c72a3bbe3fd9712aa8daa968be0c9d46
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-09-29 17:44:20 +0800

    eTutor

--- 中文題庫/數學 I/C_MM12.cpp
+++ 中文題庫/數學 I/C_MM12.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    long double i, n, t, s;
+    i = 1*100;
+    n = 2.54*30;
+    s = 0;
+
+    cin >> t;
+    t*=100;
+
+    while (s*i < t+(s*n)) {
+        s++;
+    }    
+    cout << s << '\n';
+}
