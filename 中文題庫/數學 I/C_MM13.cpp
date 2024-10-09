ee996a90fae157c6cd5a996f54405ccefeb44173
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-09 08:15:35 +0800

    簡化C_MM13

--- 中文題庫/數學 I/C_MM13.cpp
+++ 中文題庫/數學 I/C_MM13.cpp
@@ -5,15 +5,14 @@ int count(int i, int n) {
 }
 
 int main() {
-    int hour_1, minute_1, hour_2, minute_2, in_min, out_min;
+    int hour_1, minute_1, hour_2, minute_2, total;
     scanf("%d %d", &hour_1, &minute_1);
     scanf("%d %d", &hour_2, &minute_2);
 
-    in_min = count(hour_1, minute_1);
-    out_min = count(hour_2, minute_2) - in_min;
+    total = count(hour_2, minute_2) - count(hour_1, minute_1);
 
-    switch (out_min<=120 ? 1 : 2) {
-        case (1): out_min<=30 ? printf("%d\n", 30) : printf("%d\n", (out_min/30)*30); break;
-        case (2): out_min<=150 ? printf("%d\n", 160) : out_min<=240 ? printf("%d\n", (((out_min/30)-4)*40)+120) : printf("%d\n", (((out_min/30)-8)*60)+280); break;
+    switch (total<=120 ? 1 : 2) {
+        case (1): total<=30 ? printf("%d\n", 30) : printf("%d\n", (total/30)*30); break;
+        case (2): total<=150 ? printf("%d\n", 160) : total<=240 ? printf("%d\n", (((total/30)-4)*40)+120) : printf("%d\n", (((total/30)-8)*60)+280); break;
     }    
 }
