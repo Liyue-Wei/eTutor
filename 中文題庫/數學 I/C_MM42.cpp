7d6bff4994de8b2d84a6416c48d693291229d4ad
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-21 22:53:23 +0800

    C_MM42.cpp

--- 中文題庫/數學 I/C_MM42.cpp
+++ 中文題庫/數學 I/C_MM42.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 int main() {
     int i;
     long double n=0;
-    scanf("%d", i);
-    for(int t=0; t<=i; t++) {
-        n += pow((-1), (t+1))*(2*t-1);
+    while(cin >> i) {
+        for(int t=1; t<=i; t++) {
+            n += pow((-1), (t+1))*1/(2*t-1);
+        }
+        cout.setf(ios::fixed);
+        cout << setprecision(3) << n << endl;      
+        n=0;  
     }
-    cout << n;
 }
