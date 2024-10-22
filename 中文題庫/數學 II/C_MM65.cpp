1a8c1dc06f723fc7924ab60182e62b19cff0391c
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-23 02:10:37 +0800

    C_MM65
    0就是0，用甚麼"0"當敘述，是有甚麼破病

--- 中文題庫/數學 II/C_MM65.cpp
+++ 中文題庫/數學 II/C_MM65.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 int main() {
     int i, n, t, l, total, r1, r5, r50;
-    int a1=15, a2=20, a3=30;
+    const int a1=15, a2=20, a3=30;
     scanf("%d, %d, %d, %d", &i, &n, &t, &l);
 
     total = n*a1+t*a2+l*a3;
     i-=total;
-    if(i<=0) {printf("\"0\"\n");}
+    if(i<0) {printf("0\n");}
     else {
         r50 = i/50;
         i%=50;
         r5 = i/5;
         i%=5;
-        r1 = i;
+        r1 = i/1;
     
         printf("%d,%d,%d\n", r1, r5, r50);
     }
