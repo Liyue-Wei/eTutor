ce61002e4a2b2368ae40f58aedf851880cbeaef6
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-23 02:49:53 +0800

    C_MM49

--- 中文題庫/數學 I/C_MM49.cpp
+++ 中文題庫/數學 I/C_MM49.cpp
@@ -1,50 +1,23 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int i;
-    char num[10001];
-    scanf("%d", &i);
-    for(int t=0; t!=i; t++) {
-        scanf("%d", &num[t]);
+int count(int n) {
+    int num=1, ones=1;
+    while (num%n!=0) {
+        num = (num*10+1)%n;
+        ones++;
     }
-
-    for(int t=0; t!=i; t++) {
-        printf("%d ", num[t]);
-    }
-}
-
-/*
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int find_min_ones(int n) {
-    int num = 1;
-    int count = 1;
-
-    while (num % n != 0) {
-        num = (num * 10 + 1) % n;
-        count++;
-    }
-
-    return count;
+    return ones;
 }
 
 int main() {
-    int k;
-    cin >> k;
-    vector<int> results(k);
-
-    for (int i = 0; i < k; ++i) {
-        int n;
-        cin >> n;
-        results[i] = find_min_ones(n);
+    int i, n[11], num[11];
+    cin >> i;
+    for (int t=0; t<i; t++) {
+        cin >> n[t];
+        num[t] = count(n[t]);
     }
 
-    for (int i = 0; i < k; ++i) {
-        cout << results[i] << endl;
+    for (int t=0; t<i; t++) {
+        cout << num[t] << '\n';
     }
-
-    return 0;
 }
-*/
