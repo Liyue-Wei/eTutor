2c55c52a49fa43da221d3aec6ff46cb69ea5079c
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-25 01:37:28 +0800

    1025進度

--- 中文題庫/數學 II/C_MM53.cpp
+++ 中文題庫/數學 II/C_MM53.cpp
@@ -3,15 +3,16 @@ flag pi = 3.1415926535897931159979634685441851615905761718750000000000000000
 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
 */
 
-#define _USE_MATH_DEFINES
-#include <cmath>
+// #define _USE_MATH_DEFINES
+// #include <cmath>
 #include <iostream>
-#include <stdio.h>
-#include <iomanip>
+// #include <stdio.h>
+// #include <iomanip>
 using namespace std;
 int k, n[19];
-const double pi = M_PI;
+// const double pi = M_PI;
 
+/*
 int find_num(int i) {
     if (i>=9) {
         double pi_2 = (pi*(pow(10, 5)) - floor(pi*(pow(10, 5))));
@@ -24,6 +25,32 @@ int find_num(int i) {
     else {return (static_cast<long long>(pi*(pow(10, i))) - static_cast<long long>(static_cast<int>((pi*pow(10, i))/10)*10));}
     // return (static_cast<int>((pi*(pow(10, i)))), static_cast<int>((pi*(pow(10, i))/(pow(10, i)*(pow(10, i))))));
     // printf("%d", static_cast<int>(pi*(pow(10, i))) - static_cast<int>(static_cast<int>((pi*pow(10, i))/10)*10));
+}
+*/
+
+int find_num(int i) {
+    switch (i) {  // 1 4 1 5 9 2 6 5 3 5 8 9 7 9 3 1 1 5
+        case 1: return 1; break;
+        case 2: return 4; break;
+        case 3: return 1; break;
+        case 4: return 5; break;
+        case 5: return 9; break;
+        case 6: return 2; break;
+        case 7: return 6; break;
+        case 8: return 5; break;
+        case 9: return 3; break;
+        case 10: return 5; break;
+        case 11: return 8; break;
+        case 12: return 9; break;
+        case 13: return 7; break;
+        case 14: return 9; break;
+        case 15: return 3; break;
+        case 16: return 1; break;
+        case 17: return 1; break;
+        case 18: return 5; break;
+    }
+
+    return 0;
 }
 
 int main() {
