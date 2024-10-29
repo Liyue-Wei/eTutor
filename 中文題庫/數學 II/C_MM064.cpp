400aa2954604b51984b640b199510d1dbd41543d
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-29 18:04:46 +0800

    C_MM064

--- 中文題庫/數學 II/C_MM064.cpp
+++ 中文題庫/數學 II/C_MM064.cpp
@@ -7,8 +7,6 @@ using namespace std;
 int main() {
     int i, n;
     scanf("%d", &i);
-    n = ((50000 * pow(scale_1, i)) - (10000 * pow(scale_2, i)));
+    n = ((50000 * pow(scale_1, i)) - (10000 * pow(scale_2, i)))/ (35.2 * pow(scale_3, i));
     printf("%d", n);
 }
-
-//  / (35.2 * pow(scale_3, i))
