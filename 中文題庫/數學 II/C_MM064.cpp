f63cc18e05879fb9026125fad2da422d260d44db
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-10-29 14:36:01 +0800

    題目整理

---
+++ 中文題庫/數學 II/C_MM064.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#define scale_1 1.36
+#define scale_2 1.2
+#define scale_3 0.8
+using namespace std;
+int main() {
+    int i, n;
+    scanf("%d", &i);
+    n = ((50000 * pow(scale_1, i)) - (10000 * pow(scale_2, i)));
+    printf("%d", n);
+}
+
+//  / (35.2 * pow(scale_3, i))
