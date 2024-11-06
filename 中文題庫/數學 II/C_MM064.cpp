6b3bf675eb3d973bed40f47ecee0bc5e04300198
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-11-07 02:40:34 +0800

    C_MM64

--- 中文題庫/數學 II/C_MM064.cpp
+++ 中文題庫/數學 II/C_MM064.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cmath>
 #define scale_1 1.36
-#define scale_2 1.2
-#define scale_3 0.8
+#define scale_2 1.02
+#define scale_3 0.998
 using namespace std;
 int main() {
     int i, n;
-    scanf("%d", &i);
-    n = ((50000 * pow(scale_1, i)) - (10000 * pow(scale_2, i)))/ (35.2 * pow(scale_3, i));
-    printf("%d", n);
+    while (scanf("%d", &i) != EOF) {
+        n = ((50000 * pow(scale_1, i)) - (10000 * pow(scale_2, i)))/ (35.2 - 0.2 * i);
+        printf("The Company will earn %d US dollars after %d year\n", n, i);
+    }
 }
